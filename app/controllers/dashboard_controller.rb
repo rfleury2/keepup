c109549b6a6489c8212b2f07e5ca0f306a1d4a4e
Ricardo Fleury <ricardofleury@Ricardos-MacBook-Pro.local>
2016-12-28 21:26:12 -0600

    Add general refactoring around dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,9 +1,7 @@
 class DashboardController < ApplicationController
   def show
     if current_user
-      dashboard = Dashboard.new(current_user)
-      @user = dashboard.user
-      @contacts = @user.contacts
+      @dashboard = Dashboard.new(current_user)
     else
       flash[:alert] = "Log in to access your dashboard."
       redirect_to new_user_session_path
--- app/models/dashboard.rb
+++ app/models/dashboard.rb
@@ -4,4 +4,22 @@ class Dashboard
   def initialize(user)
     @user = user
   end
+
+  def upcoming_contacts(limit = 5)
+    contacts.limit(limit)
+  end
+
+  def has_no_contacts?
+    contacts.empty?
+  end
+
+  def contact_count
+    contacts.count
+  end
+
+  private 
+
+  def contacts 
+    @contacts ||= @user.contacts
+  end
 end
