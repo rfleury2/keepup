2cb6a39f7b11617c281cc8383ed59aeb30a4e344
Ricardo Fleury <ricardofleury@Ricardos-MacBook-Pro.local>
2016-12-28 20:02:23 -0600

    Set up user factory and test full_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,8 @@ class User < ActiveRecord::Base
 
   validates_presence_of :first_name, :last_name
   validates_uniqueness_of :email
+
+  def full_name
+    "#{first_name} #{last_name}" 
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,4 +8,14 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of :password }
   it { should validate_length_of(:password).is_at_least(6) }
   it { should validate_confirmation_of(:password).on(:create) }
+
+  it { should have_many :contacts }
+
+  describe '#full_name' do
+    let(:user) { FactoryGirl.build(:user, first_name: 'Joe', last_name: 'Schmo') }
+
+    it 'should concatenate first_name and last_name' do
+      expect(user.full_name).to eq "Joe Schmo"
+    end
+  end
 end
