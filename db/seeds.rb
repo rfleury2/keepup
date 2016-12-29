def9dfd096ef1cdc1a6b7aa62d44eafbba410a04
Ricardo Fleury <ricardofleury@Ricardos-MacBook-Pro.local>
2016-12-28 19:55:00 -0600

    Add basic seed with contacts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+rich_user = User.find_by(email: 'joe@user.com') || User.create!(first_name: 'Joe', last_name: 'User', email: 'joe@user.com', password: 'danpatrick', password_confirmation: 'danpatrick')
+empty_user = User.find_by(email: 'john@user.com') || User.find_or_create_by!(first_name: 'John', last_name: 'User', email: 'john@user.com', password: 'danpatrick', password_confirmation: 'danpatrick')
+
+rich_user.contacts.find_or_create_by!(name: 'Carlos Contact', email: 'carlos@contact.com')
+rich_user.contacts.find_or_create_by!(name: 'Chuck Contact', email: 'chuck@contact.com')
+rich_user.contacts.find_or_create_by!(name: 'Carla Contact', email: 'carla@contact.com')
+rich_user.contacts.find_or_create_by!(name: 'Charles Contact', email: 'charles@contact.com')
