35b08b0d9a542db00aa2de202eb960fcfdb984ae
Ricardo Fleury <ricardofleury@Ricardos-MacBook-Pro.local>
2016-12-28 22:44:18 -0600

    Fix bug with seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 rich_user = User.find_by(email: 'joe@user.com') || User.create!(first_name: 'Joe', last_name: 'User', email: 'joe@user.com', password: 'danpatrick', password_confirmation: 'danpatrick')
-empty_user = User.find_by(email: 'john@user.com') || User.find_or_create_by!(first_name: 'John', last_name: 'User', email: 'john@user.com', password: 'danpatrick', password_confirmation: 'danpatrick')
+empty_user = User.find_by(email: 'john@user.com') || User.create!(first_name: 'John', last_name: 'User', email: 'john@user.com', password: 'danpatrick', password_confirmation: 'danpatrick')
 
 rich_user.contacts.find_or_create_by!(name: 'Carlos Contact', email: 'carlos@contact.com')
 rich_user.contacts.find_or_create_by!(name: 'Chuck Contact', email: 'chuck@contact.com')
